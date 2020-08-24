38a8f90e3a63e39f713378a8a23527abcab85d33
kIvy1189 <kevindivy@gmail.com>
2020-08-24 09:34:56 -0500

    added type defs and started resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,8 @@
+const {AuthenticationError} = require('apollo-server-express');
+const {signToken} = require('../utils/auth');
+
+const resolvers = {
+    //No resolvers
+};
+
+module.exports = resolvers;
