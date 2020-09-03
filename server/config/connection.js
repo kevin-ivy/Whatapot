54539168d78b0ad3284e8b2c7d93c0ef7398d303
torreseam <615694479+torreseam@users_noreply.github.com>
2020-09-02 21:47:24 -0500

    mongolab sandbox

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/whatapot', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/whatapot', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// });
+
+    
+const connection = 'mongodb+srv://user:utabootcamp@whatapot.9j2md.mongodb.net/<dbname>?retryWrites=true&w=majority'
+//utabootcamp
+
+mongoose.connect(connection, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false })
+        .then(() => console.log("Database Connected Successfully"))
+        .catch(err => console.log(err));
+
+// mongoose.connect(MONGODB_URI || 'mongodb://localhost/whatpot', { 
+//     useNewUrlParser: true,
+//     useUnifiedTopology:  true
+//  });
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,27 @@
 const express = require('express');
 const path = require('path');
 const {ApolloServer} = require('apollo-server-express')
+// import typeDefs and resolvers
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware
 });
 
+// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({app});
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+// Serve up static assets
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
